0a04f9a48c7df27c5bd9a81affe2c6858f9b5572
David Bastos <davidtavares98@gmail.com>
2022-11-08 19:22:56 -0300

    :sparkles: feat: Item and migrate

---
+++ lib/live_view_todo/item.ex
@@ -0,0 +1,21 @@
+defmodule LiveViewTodo.Item do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias LiveViewTodo.Repo
+  alias __MODULE__
+
+  schema "items" do
+    field :person_id, :integer
+    field :status, :integer
+    field :text, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(item, attrs) do
+    item
+    |> cast(attrs, [:text, :person_id, :status])
+    |> validate_required([:text, :person_id, :status])
+  end
+end

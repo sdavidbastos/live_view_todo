017c68cf1dfea0dcdb9cfdfd182e4b677aeb30c4
David Bastos <davidtavares98@gmail.com>
2022-11-08 20:12:43 -0300

    :sparkles: feat: change static list for dynamic and add form

--- lib/live_view_todo_web/live/page_live_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule LiveViewTodoWeb.PageLiveTest do
-  use LiveViewTodoWeb.ConnCase
-  import Phoenix.LiveViewTest
-  test "disconected and connected mount", %{conn: conn} do
-    {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Disconnected"
-    assert render(page_live) =~ "What needs to be done"
-  end
-end

5525b7de546f40915ed3ad792d594d20e3d48cb5
David Bastos <davidtavares98@gmail.com>
2022-11-08 20:15:02 -0300

    :sparkles: feat: add create, get, list and update for item

--- lib/live_view_todo/item.ex
+++ lib/live_view_todo/item.ex
@@ -18,4 +18,69 @@ defmodule LiveViewTodo.Item do
     |> cast(attrs, [:text, :person_id, :status])
     |> validate_required([:text, :person_id, :status])
   end
+
+  @doc """
+  Creates a item.
+
+  ## Examples
+
+      iex> create_item(%{text: "Learn LiveView"})
+      {:ok, %Item{}}
+
+      iex> create_item(%{text: nil})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_item(attrs \\ %{}) do
+    %Item{}
+    |> changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Gets a single item.
+
+  Raises `Ecto.NoResultsError` if the Item does not exist.
+
+  ## Examples
+
+      iex> get_item!(123)
+      %Item{}
+
+      iex> get_item!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_item!(id), do: Repo.get!(Item, id)
+
+  @doc """
+  Returns the list of items.
+
+  ## Examples
+
+      iex> list_items()
+      [%Item{}, ...]
+
+  """
+  def list_items do
+    Repo.all(Item)
+  end
+
+  @doc """
+  Updates a item.
+
+  ## Examples
+
+      iex> update_item(item, %{field: new_value})
+      {:ok, %Item{}}
+
+      iex> update_item(item, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_item(%Item{} = item, attrs) do
+    item
+    |> Item.changeset(attrs)
+    |> Repo.update()
+  end
 end

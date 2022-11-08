cf1032adc94457bf144ef04bf8a55e5e430dae27
David Bastos <davidtavares98@gmail.com>
2022-11-08 17:53:39 -0300

    :sparkles: feat: page todo-app

---
+++ lib/live_view_todo_web/live/page_live.ex
@@ -0,0 +1,7 @@
+defmodule LiveViewTodoWeb.PageLive do
+  use LiveViewTodoWeb, :live_view
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+end
--- lib/live_view_todo_web/router.ex
+++ lib/live_view_todo_web/router.ex
@@ -17,7 +17,7 @@ defmodule LiveViewTodoWeb.Router do
   scope "/", LiveViewTodoWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    live "/", PageLive
   end
 
   # Other scopes may use custom stacks.
